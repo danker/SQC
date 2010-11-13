15e21cb2c951a09f10d4f803dbfc1601f61130ac
Eric Danker <eric@breomedia.com>
2010-11-12 23:32:15 -0600

    Created static method and checked for negative change.

--- src/Change.java
+++ src/Change.java
@@ -5,7 +5,21 @@ import java.math.BigDecimal;
  * $3.10, how much change do you get?
  */
 public class Change {
+
+    public static String displayCalculatedChange(BigDecimal amountTendered, BigDecimal itemCost) {
+
+        BigDecimal change = amountTendered.subtract(itemCost);
+
+        if (change.doubleValue() < 0.00) {
+            return "Insufficient funds: $" + change.abs() + " more is needed.";
+        } else {
+            return "$" + change;
+        }
+
+    }
+
     public static void main(String args[]) {
-        System.out.println("$" + new BigDecimal("4.00").subtract(new BigDecimal("3.10")));
+        System.out.println(Change.displayCalculatedChange(new BigDecimal("4.00"), new BigDecimal("3.10")));
+        System.out.println(Change.displayCalculatedChange(new BigDecimal("4.00"), new BigDecimal("5.10")));
     }
 }
